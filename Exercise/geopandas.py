6461fadacd29e8e1e544be5cf11d8a339168bce0
patlee12 <plee1@umassd.edu>
2022-04-18 02:32:58 -0400

    comment

--- Exercise/geopandas.py
+++ Exercise/geopandas.py
@@ -4,11 +4,12 @@ from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
 from descartes import PolygonPatch
 
+# import states data
 df_counties = gpd.read_file("states.json")
 
 df_counties.plot(figsize=(12, 10))
 
-# Read in income data from census
+# Read Census Data and sort out incomes
 csv = pd.read_csv("Census_Population.csv")
 df = pd.DataFrame(csv)
 df = df.loc[89].filter(like="!!Total!!Estimate")
@@ -19,8 +20,10 @@ df = df.dropna()
 
 print(df)
 
+# Save a csv copy
 df.to_csv('houseIncome.csv')
 
+# Join states data
 df_counties_data = df_counties.join(df)
 df_counties_data = df_counties_data.dropna()
 print(df_counties_data)
